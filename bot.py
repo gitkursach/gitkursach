093040184fed6a6c1cd6c48bfe0500ba04d98ff5
gitkursach <git.kursach@gmail.com>
2021-06-05 13:35:50 +0300

    BOT
    Каркас бота!

---
+++ bot.py
@@ -0,0 +1,28 @@
+#1819155073:AAGHK82SgrjwKk4BeYQwN7qiSaQTN7lleRA
+
+# API ТЕЛЕГРАММА
+import telebot
+from telebot import types
+
+import random
+import os
+import datetime
+import time
+
+# TOKEN
+token = "1819155073:AAGHK82SgrjwKk4BeYQwN7qiSaQTN7lleRA"
+bot = telebot.TeleBot(token) 
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	item1 = types.KeyboardButton("Создать новую БД.")
+	item2 = types.KeyboardButton("Войти в существующую.")
+	item3 = types.KeyboardButton("Забыл пароль")
+	markup.add(item1, item2,item3)
+
+	msg = bot.send_message(message.chat.id, f"Добро пожаловать, {message.from_user.first_name}. Данный бот создан, что бы ты мог ничего не делать!<a href='https://yandex.ru/'>Ссылка на приложение</a>",
+		parse_mode='html', reply_markup=markup)
+
+# RUN
+bot.polling(none_stop=True)
